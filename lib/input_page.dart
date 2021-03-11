14c66b996faee4f8ffe6a149639774449daf38d2
Kristopher Ong <ongkristopher@gmail.com>
2021-03-12 01:28:55 +0800

    Create widget for gender and utilize reusable widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xff1d1e33);
@@ -21,24 +24,42 @@ class _InputPageState extends State<InputPage> {
             child: Row(
           children: [
             Expanded(
-              child: ReusableCard(colour: activeCardColour,),
+              child: ReusableCard(
+                colour: activeCardColour,
+                cardChild: IconWidget(
+                  genderLabel: 'Male',
+                  genderIcon: FontAwesomeIcons.mars,
+                ),
+              ),
             ),
             Expanded(
-              child: ReusableCard(colour: activeCardColour,),
+              child: ReusableCard(
+                colour: activeCardColour,
+                cardChild: IconWidget(
+                  genderLabel: 'Female',
+                  genderIcon: FontAwesomeIcons.venus,
+                ),
+              ),
             )
           ],
         )),
         Expanded(
-          child: ReusableCard(colour: activeCardColour,),
+          child: ReusableCard(
+            colour: activeCardColour,
+          ),
         ),
         Expanded(
           child: Row(
             children: [
               Expanded(
-                child: ReusableCard(colour: activeCardColour,),
+                child: ReusableCard(
+                  colour: activeCardColour,
+                ),
               ),
               Expanded(
-                child: ReusableCard(colour: activeCardColour,),
+                child: ReusableCard(
+                  colour: activeCardColour,
+                ),
               )
             ],
           ),
@@ -50,23 +71,6 @@ class _InputPageState extends State<InputPage> {
           height: bottomContainerHeight,
         ),
       ]),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-
-  ReusableCard({@required this.colour});
-
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
